1bb14d7fda4db5d15952cfd97378772726ccc7aa
Jesse M <jmostipak@teachingtrust.org>
2018-12-14 17:20:59 -0600

    initial commit with environment set up (library calls)

---
+++ 00_pre_app_wrangle_eda.R
@@ -0,0 +1,9 @@
+#' this file is used to clean up an existing dataset as well as explore
+#' various graphs to be used in the Shiny app I'll be using for the RC
+#' Fellowship application
+
+
+# set up environment ------------------------------------------------------
+library(readr)
+library(dplyr)
+library(ggplot2)
