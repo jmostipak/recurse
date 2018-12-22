4d56fce96173c8cf3d560b1620861c79f014dbf6
Jesse M <jmostipak@teachingtrust.org>
2018-12-22 13:21:10 -0600

    replaced formatted .xls data files with .csv

---
+++ version_2.0/R/00_packages.R
@@ -0,0 +1,2 @@
+#' packages used in import -> EDA pipeline
+library(tidyverse)
--- version_2.0/R/test_file.R
+++ version_2.0/R/test_file.R
@@ -1,8 +1,16 @@
 #' objectives:
 #'   split code into relevant files
-#'   incorporate `source`
+#'   incorporate `source` if using multiple packages
+#'     >> requires local loading of `here` package
 #'   improve/modularize existing code
 #'   create initial EDA exploration (see JK resources)
 
 
+# environment set-up ------------------------------------------------------
+
+library(here)
+source(here("version_2.0/R", "00_packages.R"))
+
+# staar_data_import -------------------------------------------------------
+cref <- read_csv(here("version_2.0/data/for_use", "CREF.csv"))
 
